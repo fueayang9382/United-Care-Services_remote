fbe33d0ccf6c413b5536b3eedd32324e2b87a670
Tou Fue Yang <touyangbiz@gmail.com>
2023-06-12 17:29:48 -0500

    Initial Commit

---
+++ United Care Services/United_Care_ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  United_Care_ServicesApp.swift
+//  United Care Services
+//
+//  Created by Tou Yang on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct United_Care_ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ United Care ServicesUITests/United_Care_ServicesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  United_Care_ServicesUITestsLaunchTests.swift
+//  United Care ServicesUITests
+//
+//  Created by Tou Yang on 6/12/23.
+//
+
+import XCTest
+
+final class United_Care_ServicesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
