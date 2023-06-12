c6dfa7e343896d56bc49d530b344a5d10b9a5614
Tou Fue Yang <touyangbiz@gmail.com>
2023-06-12 17:42:02 -0500

    added second view

---
+++ United Care Services/second_view.swift
@@ -0,0 +1,20 @@
+//
+//  second_view.swift
+//  United Care Services
+//
+//  Created by Tou Yang on 6/12/23.
+//
+
+import SwiftUI
+
+struct second_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct second_view_Previews: PreviewProvider {
+    static var previews: some View {
+        second_view()
+    }
+}
