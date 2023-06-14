2301d95e68f927d5349f837b860f36b1bdf0b465
Tou Fue Yang <touyangbiz@gmail.com>
2023-06-13 19:05:34 -0500

    created test folder

---
+++ Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  United Care Services
+//
+//  Created by Tou Yang on 6/13/23.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
